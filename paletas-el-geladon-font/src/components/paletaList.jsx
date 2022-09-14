8cf9856016bff9dfd607c531b8d14190f3f008d4
jardejane <janefeitosalima49@gmail.com>
2022-09-14 13:44:08 -0300

    feat: add the logic on the button

--- paletas-el-geladon-font/src/components/paletaList.jsx
+++ paletas-el-geladon-font/src/components/paletaList.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import { paletas } from "mocks/paletas";
 import "./paletaList.css";
 
 function PaletaLista() {
+  const [paletaSelecionada, setpaletaSelecionada] = useState({});
+
+  const adicionarItem = (paletaIndex) =>{
+    const paleta = {[paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) +1 }
+    setpaletaSelecionada({...paletaSelecionada, ...paleta})
+  }
+
   return (
     <div className="PaletaLista">
       {paletas.map((paleta, index) => (
         <div className="PaletaListaItem" key={`PaletalistaItem-${index}`}>
-
+          <span className="PaletaListaItem__badge">{paletaSelecionada[index] || 0}</span>
           <div>
             <div className="PaletaListaItem__titulo">{paleta.titulo}</div>
             <div className="PaletaListaItem__preco"> R$ {paleta.preco}</div>
@@ -15,7 +23,7 @@ function PaletaLista() {
               {paleta.descricao}{" "}
             </div>
             <div className="PaletaListaItem__acoes Acoes">
-              <button className="Acoes__adicionar Acoes__adicionar--preencher">
+              <button className="Acoes__adicionar Acoes__adicionar--preencher" onClick={  () => adicionarItem(index)}>
                 adicionar
               </button>
             </div>
