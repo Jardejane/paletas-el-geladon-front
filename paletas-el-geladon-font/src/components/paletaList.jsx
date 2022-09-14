485a8776c3a3f7336ef0494923a1e2ed45cd5d94
jardejane <janefeitosalima49@gmail.com>
2022-09-13 21:42:53 -0300

    feat: add the key

--- paletas-el-geladon-font/src/components/paletaList.jsx
+++ paletas-el-geladon-font/src/components/paletaList.jsx
@@ -5,11 +5,15 @@ function PaletaLista() {
   return (
     <div className="PaletaLista">
       {paletas.map((paleta, index) => (
-        <div className="PaletaListaItem">
+        <div className="PaletaListaItem" key={`PaletalistaItem-${index}`}>
+
           <div>
             <div className="PaletaListaItem__titulo">{paleta.titulo}</div>
             <div className="PaletaListaItem__preco"> R$ {paleta.preco}</div>
-            <div className="PaletaListaItem__descricao"> {paleta.descricao} </div>
+            <div className="PaletaListaItem__descricao">
+              {" "}
+              {paleta.descricao}{" "}
+            </div>
             <div className="PaletaListaItem__acoes Acoes">
               <button className="Acoes__adicionar Acoes__adicionar--preencher">
                 adicionar
@@ -19,6 +23,7 @@ function PaletaLista() {
           <img
             className="PaletaListaItem__foto"
             src={paleta.foto}
+            alt={`paleta de ${paleta.sabor}`}
           />
         </div>
       ))}
