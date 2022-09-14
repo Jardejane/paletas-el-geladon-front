38561f54e73638daa02e4e41439408338cfc23ec
jardejane <janefeitosalima49@gmail.com>
2022-09-13 21:21:54 -0300

    feat: creat mocks and config initial

---
+++ paletas-el-geladon-font/src/components/home.jsx
@@ -0,0 +1,13 @@
+import "./home.css";
+import  PaletaLista from "./paletaList";
+
+function Home() {
+  return (
+  <div className="Home">
+    <div className="Home__container">
+    < PaletaLista />
+    </div>
+  </div>
+  );
+}
+export default Home;
