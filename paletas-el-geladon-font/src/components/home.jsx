d9912792c4029b94e4c9afc78e8269bfaea53e59
jardejane <janefeitosalima49@gmail.com>
2022-09-14 13:04:27 -0300

    feat: add icones e logo no home

--- paletas-el-geladon-font/src/components/home.jsx
+++ paletas-el-geladon-font/src/components/home.jsx
@@ -1,13 +1,39 @@
 import "./home.css";
-import  PaletaLista from "./paletaList";
+import PaletaLista from "./paletaList";
+import sacola from "../assets/icons/sacola.svg";
+import logo from "../assets/logo.svg";
 
 function Home() {
   return (
-  <div className="Home">
-    <div className="Home__container">
-    < PaletaLista />
+    <div className="Home">
+      <div className="Home__header Header">
+        <div className="row">
+          <div className="Header__logo Logo">
+            <img
+              src={logo}
+              width="70px"
+              alt="Logo El Geladon"
+              className="Logo__icone"
+            />
+            <span className="Logo__titulo"> El Geladon </span>
+          </div>
+          <div className="Header__opcoes Opcoes">
+            <div className="opcoes__sacola sacola">
+              <img
+                src={sacola}
+                width="40px"
+                className="Sacola__icone"
+                alt="sacola do El Geladon"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="Home__container">
+        <PaletaLista />
+      </div>
     </div>
-  </div>
   );
 }
 export default Home;
