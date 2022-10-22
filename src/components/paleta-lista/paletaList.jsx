1aca9bdf35571312a7a5dcc2cb0f17addbedee66
jardejane <janefeitosalima49@gmail.com>
2022-10-22 13:23:06 -0300

    subdivide components

--- src/components/paleta-lista/paletaList.jsx
+++ src/components/paleta-lista/paletaList.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { PaletaListaItem } from "../paleta.lista.item/paleta.lista.item";
 import { paletas } from "mocks/paletas";
 import "./paletaList.css";
 
@@ -14,36 +15,11 @@ function PaletaLista() {
    setpaletaSelecionada({...paletaSelecionada, ...paleta})
   }
 
-  const badgeCounter = (canRender, index) =>
-          Boolean(canRender) &&  (<span className="PaletaListaItem__badge">{paletaSelecionada[index]} </span>)
-
-  const removerButton = (canRender, index) => 
-        Boolean(canRender) && (<button className="Acoes__remover" onClick={() => removerItem(index)}>remover</button>)
+  
   return (
     <div className="PaletaLista">
       {paletas.map((paleta, index) => (
-        <div className="PaletaListaItem" key={`PaletalistaItem-${index}`}>
-          {badgeCounter(paletaSelecionada[index], index)}
-          <div>
-            <div className="PaletaListaItem__titulo">{paleta.titulo}</div>
-            <div className="PaletaListaItem__preco"> R$ {paleta.preco}</div>
-            <div className="PaletaListaItem__descricao">
-              {" "}
-              {paleta.descricao}{" "}
-            </div>
-            <div className="PaletaListaItem__acoes Acoes">
-              <button className= {`Acoes__adicionar ${!paletaSelecionada[index] && `Acoes__adicionar--preencher`}`}  onClick={  () => adicionarItem(index)}>
-                adicionar
-              </button>
-              {removerButton(paletaSelecionada[index], index)}
-            </div>
-          </div>
-          <img
-            className="PaletaListaItem__foto"
-            src={paleta.foto}
-            alt={`paleta de ${paleta.sabor}`}
-          />
-        </div>
+       <PaletaListaItem key={`PaletaListaItem ${index}`} />
       ))}
     </div>
   );
